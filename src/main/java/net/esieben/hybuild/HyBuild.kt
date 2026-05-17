5d32f8689ada26d082536361dd31d544cbdacd34
E-Sieben <business@esieben.net>
2026-05-17 15:12:12 +0200

    feat: added Lombok and the HytaleServer.jar as a Dependency

--- src/main/java/net/esieben/hybuild/HyBuild.kt
+++ src/main/java/net/esieben/hybuild/HyBuild.kt
@@ -13,6 +13,7 @@ class HyBuild : Plugin<Project> {
 
     companion object {
         const val PLUGIN_FOLDER = ".hytale"
+        const val LOMBOK_VERSION = "1.18.36"
     }
 
     val className: String = this::class.java.simpleName
@@ -21,8 +22,31 @@ class HyBuild : Plugin<Project> {
         val logger = project.logger
         logger.lifecycle("$className Plugin is starting")
 
-
         /// Server
+        registerServerTasks(project)
+
+        /// Project Setups
+        registerProjectTasks(project)
+
+        /// Dependencies
+        project.plugins.withId("java") {
+            project.dependencies.add("compileOnly", project.files("$PLUGIN_FOLDER/HytaleServer.jar"))
+            project.dependencies.add("compileOnly", "org.projectlombok:lombok:$LOMBOK_VERSION")
+            project.dependencies.add("annotationProcessor", "org.projectlombok:lombok:$LOMBOK_VERSION")
+        }
+    }
+
+    private fun registerProjectTasks(project: Project) {
+        project.tasks.register(
+            "addHytaleFolderToGitignore",
+            AddHytaleFolderToGitignoreTask::class.java
+        ) {
+            it.group = "hytale project"
+            it.description = "Adds the .hytale folder to your gitignore file"
+        }
+    }
+
+    private fun registerServerTasks(project: Project) {
         val downloadServerDownloaderTask = project.tasks.register(
             "downloadServerDownloader",
             DownloadServerDownloaderTask::class.java
@@ -60,15 +84,6 @@ class HyBuild : Plugin<Project> {
             it.group = "hytale server"
             it.description =
                 "Starts the Hytale Server, downloading and extracting server files first if needed"
-        }
-
-        /// Project Setups
-        project.tasks.register(
-            "addHytaleFolderToGitignore",
-            AddHytaleFolderToGitignoreTask::class.java
-        ) {
-            it.group = "hytale project"
-            it.description = "Adds the .hytale folder to your gitignore file"
         }
     }
 }
